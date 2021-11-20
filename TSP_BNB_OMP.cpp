ddda268ad860ddc887a4dd15480e4dfcf5bb7514
Student 235093 <235093@student.pwr.edu.pl>
2021-11-20 13:31:41 +0100

    Add project files.

---
+++ TSP_BNB_OMP.cpp
@@ -0,0 +1,17 @@
+﻿// TSP_BNB_OMP.cpp : Defines the entry point for the application.
+//
+
+#include "TSP_BNB_OMP.h"
+#include "omp.h"
+
+using namespace std;
+int main()
+{
+	cout << "Hello CMake." << endl;
+#pragma omp parallel
+	{
+		printf("Hello world from thread = %d\n", omp_get_thread_num());
+
+	}
+	return 0;
+}
